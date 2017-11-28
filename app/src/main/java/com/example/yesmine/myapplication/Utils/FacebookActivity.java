9f095b96c89fa7c876f492883c0abfca976c198e
yosryekken <yosr.yekken@esprit.tn>
2017-11-28 02:09:14 +0000

    api facebook

---
+++ app/src/main/java/com/example/yesmine/myapplication/Utils/FacebookActivity.java
@@ -0,0 +1,15 @@
+package com.example.yekkenyosr.pi.utils;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yekkenyosr.pi.R;
+
+public class FacebookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facebook);
+    }
+}
